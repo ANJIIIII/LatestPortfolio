b6d74fb630cc081df471ce17b168dc83701fc812
Anjali Mishra <110275402+ANJIIIII@users.noreply.github.com>
2024-09-23 21:27:11 +0530

    Update navbar.jsx

--- developer-portfolio-main/app/components/navbar.jsx
+++ developer-portfolio-main/app/components/navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
           <Link
             href="/"
             className=" text-[#16f2b3] text-3xl font-bold">
-            ANNI&#39;s WEB
+            ANNI WEB
           </Link>
         </div>
 
